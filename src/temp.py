fd5dde6ee2846e8980f4a8ebd43d936ef1de98bb
Takuma Fujitsuka <fujitsuka@uec.ac.jp>
2014-05-29 18:38:37 +0900

    殴り書き用ファイル追加

---
+++ src/temp.py
@@ -0,0 +1,33 @@
+def getDistance(transactionList, pattern):
+    # トランザクション一つに含まれるパターンの距離を返す
+    for i in len(pattern):
+        for j in len():
+            pass
+    return dist
+
+def getDistanceList(transactionList, pattern):
+    # パターンの距離をリストで返す
+    for i in len(transactionList):
+        dist_list[getDistance(transactionList, i)] += 1
+    return dist_list
+
+def CountAll(transactionList, pattern):
+    # パターンの出現数を返す, dist_list呼ばずに値保持して参照するだけにしたい
+    dist_list = getDistanceList(transactionList, pattern)
+    for i in len(dist_list):
+        count += dist_list[i]
+    return count
+
+def getDistanceRate(transactionList, pattern):
+    # パターンの距離を基に距離割合を返す
+    count = CountAll(transactionList, pattern)
+    for i in count:
+        if :
+            weight_sum = weight_sum + discount**distance * weight
+    return dist_rate = weight_sum / countAll(transactionList, pattern)
+
+def getNeighborhood(item):
+    # パターンの距離割合を基に隣接度を返す
+    dist_rate = getDistanceRate()
+    neighborhood = dist_rate * subsetsum
+    return neighborhood
