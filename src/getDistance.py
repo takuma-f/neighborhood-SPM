433498cde715d8075c966d992dbd3dd7db27d06e
Takuma Fujitsuka <fujitsuka@uec.ac.jp>
2014-05-31 05:55:09 +0900

    先頭がヒットしない場合について対応 何もヒットしない場合の処理が難しい…

--- src/getDistance.py
+++ src/getDistance.py
@@ -1,25 +1,26 @@
 # coding: UTF-8
-
 def getDistance(pattern, record):
-	# レコード一つに含まれるパターンの距離を返す
-	pass
+    # レコード一つに含まれるパターンの距離を返す
+    result    = 0
+    distance  = 0
+    pdistance = 0
+    firsthit  = False
+
+    for p in pattern:
+        for r in record:
+            if p == r:
+                temp      = record.index(r) - pattern.index(p)
+                distance  = temp - pdistance
+                pdistance = temp
+                if firsthit == False:
+                    distance = 0
+                    firsthit = True
+                result += distance
+    return result
+
 
 if __name__ == '__main__':
-	result    = 0
-	distance  = 0
-	pdistance = 0
-	firsthit  = False
-	pattern = [       "Eat",       "Tea",       "Bar"]
-	record  = ["Play","Eat","Shop","Tea","Play","Bar"]
+	pattern = [       "Eat","Tea","Bar"]
+	record  = ["Play","Eat","Tea","Bar"]
 
-	for x in pattern:
-		for y in record:
-			if x == y:
-				temp      = record.index(y) - pattern.index(x)
-				distance  = temp - pdistance
-				pdistance = temp
-				if firsthit == False:
-					distance = 0
-					firsthit = True
-				result += distance
-	print result
+	print getDistance(pattern,record)
--- src/temp.py
+++ src/temp.py
@@ -1,37 +1,78 @@
-def getDistance(transactionList, pattern):
-    # トランザクション一つに含まれるパターンの距離を返す
-    for i in len(pattern):
-        for j in len(transaction):
-            if (i != 0) and (dist[0] == 0):
-                dist = 0
-            elif (i != 0) and (transaction[i] == pattern[j]):
-                dist[i] += 1
-            elif 
-    return dist
+# coding: UTF-8
+def getDistance(record, pattern):
+    # レコード一つに含まれるパターンの距離を返す
+    distance_sum = 0
+    p_diff       = 0
+    count_p        = 0
+    firsthit     = False
+
+    for p in pattern:    # パターン内の行動の個数回す
+        print "p =",p
+        for r in record: # レコード内の行動の個数回す
+            print "r =",r
+            if p == r:   # パターン内とレコード内の行動が一致した場合
+                diff      = record.index(r) - pattern.index(p)
+                distance  = diff - p_diff
+                p_diff = diff
+                if count_p == 0 and firsthit == False:
+                    distance = 0
+                    firsthit = True
+                distance_sum += distance
+        count_p += 1
+    if firsthit == False:
+        return "None" # 何も一致しない場合Noneを返す
+    return distance_sum
 
 def getDistanceList(transactionList, pattern):
-    # パターンの距離をリストで返す
-    for i in len(transactionList):
-        dist_list[getDistance(transactionList, i)] += 1
-    return dist_list
+    # 全てのトランザクションについて距離を求め, パターンごとに距離リストを返す
+
+    distance_list = [0,0,0] # とりあえずあとで空のリストでアレする方法アレ
+    for record in transactionList:
+        print record
+        distance = getDistance(record, pattern)
+        print distance
+        if distance != "None": # 0 = False扱い？
+            distance_list[distance] += 1
+    return distance_list
 
 def CountAll(transactionList, pattern):
     # パターンの出現数を返す, dist_list呼ばずに値保持して参照するだけにしたい
-    dist_list = getDistanceList(transactionList, pattern)
-    for i in len(dist_list):
-        count += dist_list[i]
-    return count
+    # dist_list = getDistanceList(transactionList, pattern)
+    # for i in len(dist_list):
+    #     count += dist_list[i]
+    # return count
+    pass
 
 def getDistanceRate(transactionList, pattern):
     # パターンの距離を基に距離割合を返す
-    count = CountAll(transactionList, pattern)
-    for i in count:
-        if :
-            weight_sum = weight_sum + discount**distance * weight
-    return dist_rate = weight_sum / countAll(transactionList, pattern)
+    # count = CountAll(transactionList, pattern)
+    # for i in count:
+    #     if :
+    #         weight_sum = weight_sum + discount**distance * weight
+    # return dist_rate = weight_sum / countAll(transactionList, pattern)
+    pass
 
 def getNeighborhood(transactionList, pattern):
     # パターンの距離割合を基に隣接度を返す
-    dist_rate = getDistanceRate(transactionList, pattern)
-    neighborhood = dist_rate * subsetsum
-    return neighborhood
+    # dist_rate = getDistanceRate(transactionList, pattern)
+    # neighborhood = dist_rate * subsetsum
+    # return neighborhood
+    pass
+
+if __name__ == '__main__':
+    transactionList = [
+    ["Eat","Bar"],
+    ["Tea","Shop","Eat","Play"],
+    ["Tea","Shop","Eat","Bar"],
+    ["Eat","Bar"],
+    ["Eat","Play"],
+    ["Play","Bar"],
+    ["Eat","Bar"],
+    ["Shop","Eat","Play","Bar"],
+    ["Shop","Play"]
+    ]
+    pattern = ["Shop","Play"] # 存在しないパターン書くとなんかバグ出る
+
+    # 期待する結果[1, 2, 0]
+
+    print getDistanceList(transactionList,pattern)
