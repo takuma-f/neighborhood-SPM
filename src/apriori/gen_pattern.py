a8ce19cf8b9d010e6c01a82a3d3967853da9a9da
Takuma Fujitsuka <fujitsuka@uec.ac.jp>
2014-06-09 19:49:36 +0900

    間違えてディレクトリ削除した…

--- src/apriori/gen_pattern.py
+++ src/apriori/gen_pattern.py
@@ -23,7 +23,6 @@ def subsets(arr):
     """ Returns non empty subsets of arr"""
     return chain(*[combinations(arr,i + 1) for i,a in enumerate(arr)])
 
-
 def returnItemsWithMinSupport(itemSet, transactionList, minSupport, freqSet):
     """calculates the support for items in the itemSet and returns a subset of the itemSet 
     each of whose elements satisfies the minimum support"""
@@ -44,23 +43,19 @@ def returnItemsWithMinSupport(itemSet, transactionList, minSupport, freqSet):
 
     return _itemSet
 
-
-
 def joinSet(itemSet,length):
     """Join a set with itself and returns the n-element itemsets"""
     return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
 
-
 def getItemSetTransactionList(data_iterator):
-    transactionList = list()
-    itemSet         = set()
+    transactionList = list() # 履歴の集合を格納する
+    itemSet         = set() # 行動の集合を格納する
     for record in data_iterator:
         transaction = frozenset(record)
         transactionList.append(transaction)
         for item in transaction:
             itemSet.add(frozenset([item])) # Generate 1-itemSets
     return itemSet, transactionList
-
 
 def runApriori(data_iter, minSupport, minConfidence):
     """
@@ -70,7 +65,7 @@ def runApriori(data_iter, minSupport, minConfidence):
      - rules ((pretuple, posttuple), confidence)
     """
     itemSet, transactionList = getItemSetTransactionList(data_iter)
-    
+
     freqSet     = defaultdict(int)
     largeSet    = dict() # Global dictionary which stores (key=n-itemSets,value=support) which satisfy minSupport
     assocRules  = dict() # Dictionary which stores Association Rules
@@ -86,43 +81,11 @@ def runApriori(data_iter, minSupport, minConfidence):
         currentLSet     = currentCSet
         k = k + 1
 
-    def getSupport(item):
-            """local function which Returns the support of an item"""
-            return float(freqSet[item])/len(transactionList)
-
-    toRetItems=[]
+    patternList=list()
     for key,value in largeSet.items():
-        toRetItems.extend(list(item) for item in value)
+        patternList.extend(list(item) for item in value)
 
-    # toRetRules=[]
-    # for key,value in largeSet.items()[1:]:
-    #     for item in value:
-    #         _subsets = map(frozenset,[x for x in subsets(item)])
-    #         for element in _subsets:
-    #             remain = item.difference(element)
-    #             if len(remain)>0:
-    #                 confidence = getSupport(item)/getSupport(element)
-    #                 if confidence >= minConfidence:
-    #                     toRetRules.append(((tuple(element),tuple(remain)), confidence))
-    # return toRetItems, toRetRules
-    return toRetItems
-
-
-
-def printResults(items):
-    """prints the generated itemsets and the confidence rules"""
-    for pattern in items:
-        print "Pattern : %s" % str(pattern)
-
-# def printResults(items, rules):
-#     """prints the generated itemsets and the confidence rules"""
-#     for item, support in items:
-#         print "item: %s , %.3f" % (str(item), support)
-#     print "\n------------------------ RULES:"
-#     for rule, confidence in rules:
-#         pre, post = rule
-#         print "Rule: %s ==> %s , %.3f" % (str(pre), str(post), confidence)
-
+    return transactionList, patternList
 
 def dataFromFile(fname):
     """Function which reads from the file and yields a generator"""
@@ -132,7 +95,10 @@ def dataFromFile(fname):
         record = frozenset(line.split(','))
         yield record
 
-
+def printResults(items):
+    """prints the generated itemsets and the confidence rules"""
+    for pattern in items:
+        print "Pattern : %s" % str(pattern)
 
 if __name__ == "__main__":
 
@@ -156,5 +122,4 @@ if __name__ == "__main__":
     minConfidence = options.minC
     items  = runApriori(inFile, minSupport, minConfidence)
 
-    # print items
     printResults(items)
