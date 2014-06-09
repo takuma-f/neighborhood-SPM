9c87a76b61ec2fa7796e9860d73580cb594951f9
Takuma Fujitsuka <fujitsuka@uec.ac.jp>
2014-06-09 19:52:29 +0900

    実行用ファイル作成

--- src/run.py
+++ src/run.py
@@ -1,6 +1,29 @@
 # coding: UTF-8
-import gen_pattern
-import neighborhood
+from apriori      import gen_pattern
+from neighborhood import neighborhood
+from optparse     import OptionParser
+
+import sys
+import re
 
 if __name__ == '__main__':
-	main()
+    optparser = OptionParser()
+    optparser.add_option('-f', '--inputFile', dest = 'input', help = 'the filename which contains the comma separated values', default=None)
+
+    (options, args) = optparser.parse_args()
+
+    inFile = None
+    if options.input is None:
+        inFile = sys.stdin
+    elif options.input is not None:
+        inFile = gen_pattern.dataFromFile(options.input)
+    else:
+        print 'No dataset filename specified, system with exit\n'
+        sys.exit('System will exit')
+
+    minSupport    = 0.0
+    minConfidence = 0.0
+    transactionList, patternList = gen_pattern.runApriori(inFile, minSupport, minConfidence)
+    for pattern in patternList:
+        score = neighborhood.getScore(transactionList, pattern)
+        print "Pattern:%s Score:%s" % (pattern,score)
