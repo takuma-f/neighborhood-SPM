ab33e989d282f27780a748c9742fc30b2373c802
Takuma Fujitsuka <fujitsuka@uec.ac.jp>
2014-06-23 03:23:37 +0900

    モデル生成モジュール作成

--- src/model/model.py
+++ src/model/model.py
@@ -4,24 +4,15 @@ sys.path.append('/Library/Python/2.7/site-packages/libsvm-3.17/python')
 from svm import *
 from svmutil import *
 
-# 学習データ入力
-y,x = svm_read_problem('takuma.txt')
+y,x = svm_read_problem('takuma.txt') # 学習データ入力
+prob = svm_problem(y,x) # 学習データ読み込み
+param = svm_parameter('-t 2 -c 1') # パラメータ設定
+m = svm_train(prob,param) # 学習, 分類モデル作成
+svm_save_model('model_file.svm', m) # モデルをファイルに書き出す
 
-# 学習データ読み込み
-prob = svm_problem(y,x)
+yt, xt = svm_read_problem('takuma.txt') # 比較対象の学習データを読み込む
 
-# パラメータ設定
-param = svm_parameter('-t 1 -c 3')
-
-# 学習, 分類モデル作成
-m = svm_train(prob,param)
-
-# モデルをファイルに書き出す
-svm_save_model('model_file', m)
-
-# 比較対象ユーザーの学習データを読み込む
-yt, xt = svm_read_problem('kamui.txt')
-
-#予測する
-p_label, p_acc, p_val = svm_predict(yt, xt, m )
-print p_acc
+p_label, p_acc, p_val = svm_predict(yt, xt, m ) #予測する
+# print p_label
+# print p_acc
+# print p_val
